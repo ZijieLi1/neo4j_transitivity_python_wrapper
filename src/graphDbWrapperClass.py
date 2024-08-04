7ae090f1c29902dadc030262f7ea303d4c976b06
ZijieLi <zijie.li@outlook.com>
2024-08-04 12:32:07 +1000

    remove tested code

--- src/graphDbWrapperClass.py
+++ src/graphDbWrapperClass.py
@@ -5,12 +5,6 @@ class Neo4jWrapper(GraphDatabase):
     def driver(uri: str, auth: tuple):
         driver = GraphDatabase.driver(uri, auth=auth)
         return driver_init(driver)
-# Example usage
 
 
-if __name__ == "__main__":
-    uri = "bolt://localhost:7687"
-    auth = ("neo4j", "0000000000")
-    # Initialize the wrapper
-    db = Neo4jWrapper.driver(uri, auth=auth)
-    print(db.execute_query("RETURN 'hello' AS message"))
+
